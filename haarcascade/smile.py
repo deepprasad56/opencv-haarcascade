49bfaedb2f9f544513dddac282a1733a36c65612
Kumar Shivam <36999935+kshivam99@users.noreply.github.com>
2018-04-07 17:48:44 +0530

    Update smile.py

--- haarcascade/smile.py
+++ haarcascade/smile.py
@@ -16,6 +16,7 @@ while True:
 
     for (x,y,w,h) in faces:
         cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
+        
 
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
@@ -29,6 +30,9 @@ while True:
 
         for (ex, ey, ew, eh) in smile:
             cv2.rectangle(roi_color, (ex,ey), (ex+ew, ey+eh), (0,255,255), 2)
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            cv2.putText(img,'Smile',(w,h), font, 0.5, (11,255,255), 2, cv2.LINE_AA)
+
 
     cv2.imshow('img', img)
     k = cv2.waitKey(30) & 0xff
